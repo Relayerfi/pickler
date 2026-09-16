91863130211ce41ac86dee4105430fbde74a3633
Gilberts Ahumada <gilbertsahumada@gmail.com>
2026-09-16 10:57:42 +0200

    style: format packages/core/src/index.ts

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,6 +1,6 @@
 export type { Clock } from "./ports/clock.js";
 export { createGetHealth } from "./application/get-health.js";
-export * from './features/research/types.js';
-export { createResearchRunner } from './features/research/run-research.js';
+export * from "./features/research/types.js";
+export { createResearchRunner } from "./features/research/run-research.js";
 
-export * from './features/research/policy.js';
+export * from "./features/research/policy.js";
