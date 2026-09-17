0b038682e15d18872614e2762e5ca9cd4488ee2a
Gilberts Ahumada <gilbertsahumada@gmail.com>
2026-09-17 17:23:15 +0200

    Add configurable NFL research: packages/core/src/index.ts

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,5 @@
+export * from "./features/research/plugins.js";
+export * from "./features/research/nfl.js";
 export type { Clock } from "./ports/clock.js";
 export { createGetHealth } from "./application/get-health.js";
 export * from "./features/research/types.js";
